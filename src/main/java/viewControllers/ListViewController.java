acf0e4183faa607935a9f734ef577f2c37b55df6
gerome <gerome.meyer@icloud.com>
2018-11-07 14:25:00 +0100

    EntryCreationController updated;

--- src/main/java/viewControllers/ListViewController.java
+++ src/main/java/viewControllers/ListViewController.java
@@ -1,16 +1,23 @@
 package viewControllers;
 
 
+import encryption.CryptionManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import models.Entry;
 import models.User;
+
+import java.io.IOException;
 
 public class ListViewController extends ViewController {
 
@@ -18,9 +25,6 @@ public class ListViewController extends ViewController {
 
     @FXML
     private VBox contentBox;
-
-    public ListViewController() {
-    }
 
     public void setUp(Scene scene, User user) {
         scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
@@ -30,11 +34,12 @@ public class ListViewController extends ViewController {
             }
         });
         currentUser = user;
+        CryptionManager cm = new CryptionManager(currentUser.getPassword());
         for(Entry e : currentUser.getEntries()) {
             Label username = new Label(e.getUsername());
             username.getStyleClass().add("listLabel");
 
-            Label password = new Label(e.getPassword());
+            Label password = new Label(cm.decrypt(e.getPassword()));
             password.getStyleClass().add("listLabel");
 
             VBox box = new VBox(username, password);
@@ -50,11 +55,27 @@ public class ListViewController extends ViewController {
 
     @FXML
     public void handleButtonNew(ActionEvent e) {
-        loadScreen("/fxmlFiles/entryCreation.fxml", e, currentUser);
+        loadScreen("/fxmlFiles/entryCreation.fxml", currentUser);
     }
 
     @FXML
     public void handleButtonHelp(ActionEvent e) {
         // TODO: Open box with help commands
+    }
+
+    public void loadScreen(String screen, User user) {
+        Parent root;
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(screen));
+            root = loader.load();
+            EntryCreationController entryCreationController = loader.getController();
+            entryCreationController.setUp(user);
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
     }
 }
